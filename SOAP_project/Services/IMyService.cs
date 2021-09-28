23b8073663068c611f65e936865881aaf3fce022
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2021-09-28 15:45:35 +0300

    Add project files.

---
+++ SOAP_project/Services/IMyService.cs
@@ -0,0 +1,21 @@
+﻿using SOAP_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SOAP_project.Services
+{
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        string Test(string s);
+       
+        [OperationContract]
+        UserModel TestUserModel(UserModel model);
+    }
+}
---
+++ SOAP_project/Services/MyService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SOAP_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace SOAP_project.Services
+{
+    public class MyService:IMyService
+    {
+        public string Test(string s)
+        {
+            return "Your message: "+ s;
+        }
+
+
+        public UserModel TestUserModel(UserModel customModel)
+        {
+             return customModel;
+        }
+    }
+}
